dcbc07f5b6fc97e5c6a873c6c4ebd073dccb7103
Chris Hartman <chris.m.hartman@gmail.com>
2020-12-02 10:39:44 -0900

    Clean up day 2

--- day2.cpp
+++ day2.cpp
@@ -1,43 +1,12 @@
-
-#include <algorithm>
-#include <cmath>
-#include <cstdlib>
 #include <fstream>
-#include <iomanip>
 #include <iostream>
-#include <iterator>
-#include <map>
-#include <memory>
-#include <numeric>
-#include <queue>
-#include <set>
 #include <sstream>
 #include <string>
-#include <tuple>
-#include <utility>
-#include <vector>
-using std::abs;
-using std::ceil;
-using std::count;
 using std::cout;
-using std::endl;
-using std::forward_as_tuple;
 using std::ifstream;
 using std::istream;
 using std::istringstream;
-using std::map;
-using std::max;
-using std::max_element;
-using std::min;
-using std::pair;
-using std::queue;
-using std::set;
-using std::sqrt;
 using std::string;
-using std::swap;
-using std::tie;
-using std::tuple;
-using std::vector;
 
 void day2() {
    auto star1 = 0;
@@ -63,8 +32,3 @@ void day2() {
    cout << "Day 2 star 1 = " << star1 << "\n";
    cout << "Day 2 star 2 = " << star2 << "\n";
 }
-/*
- * 4-5 r: rrrjr
-9-10 x: pxcbpxxwkqjttx
-8-13 b: rjbbbbvgrbrfjx
- */
