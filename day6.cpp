99e88e94bcedd014d728b0b277e52b7186f61eb6
Chris Hartman <chris.m.hartman@gmail.com>
2020-12-06 11:43:47 -0900

    Clean up day 6

--- day6.cpp
+++ day6.cpp
@@ -1,73 +1,42 @@
 #include <algorithm>
-#include <cmath>
-#include <cstdlib>
 #include <fstream>
 #include <iomanip>
 #include <iostream>
 #include <iterator>
-#include <map>
-#include <memory>
-#include <numeric>
-#include <queue>
 #include <set>
-#include <sstream>
-#include <string>
-#include <tuple>
-#include <utility>
-#include <vector>
-using std::abs;
-using std::ceil;
-using std::ceil;
+using std::back_inserter;
 using std::cout;
-using std::endl;
 using std::ifstream;
 using std::istream;
-using std::istringstream;
-using std::map;
-using std::max;
-using std::max_element;
-using std::min;
-using std::pair;
 using std::set;
-using std::queue;
-using std::sqrt;
+using std::set_intersection;
 using std::string;
-using std::forward_as_tuple;
-using std::tie;
-using std::tuple;
-using std::swap;
-using std::vector;
+
 
 void day6() {
    auto star1 = 0;
    auto star2 = 0;
+   string groupAllYes = "abcdefghijklmnopqrstuvwxyz";
+   set<char> groupAnyYes;
+
    ifstream ifile("../day6.txt");
    string line;
-   set<char> ans;
-   bool first=true;
    while (getline(ifile, line)) {
-      if(line == "") {
-         star2 += ans.size();
-         ans.clear();
-         first = true;
-      }
-      else {
-         set<char> curPerson;
-         for (auto c : line)
-            curPerson.insert(c);
-         if (first) {
-            ans = curPerson;
-            first = false;
-         }
-         else {
-            vector<char> newAns;
-            std::set_intersection(ans.begin(),ans.end(),curPerson.begin(),curPerson.end(),std::back_inserter(newAns));
-            ans.clear();
-            ans.insert(newAns.begin(),newAns.end());
-         }
+      if (line.empty() || ifile.eof()) {
+         star1 += groupAnyYes.size();
+         groupAnyYes.clear();
+         star2 += groupAllYes.size();
+         groupAllYes = "abcdefghijklmnopqrstuvwxyz";
+      } else {
+         groupAnyYes.insert(line.begin(), line.end());
+         sort(line.begin(),line.end());
+         string intersection;
+         set_intersection(groupAllYes.begin(),groupAllYes.end(),
+                               line.begin(),line.end(),
+                               back_inserter(intersection));
+         groupAllYes = intersection;
       }
    }
-
    cout << "Day 6 star 1 = " << star1 << "\n";
    cout << "Day 6 star 2 = " << star2 << "\n";
 }
