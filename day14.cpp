ac9697485f1f6a44e73632cd1867c7040f0bbf7c
Chris Hartman <chris.m.hartman@gmail.com>
2020-12-13 20:40:16 -0900

    Day 14 both stars

--- day14.cpp
+++ day14.cpp
@@ -35,47 +35,65 @@ using std::string;
 using std::swap;
 using std::tie;
 using std::tuple;
+
 using std::vector;
+
+map<long long, long long> star2memory;
+
+void handleStar2Mask(const string &mask, long long m, long long v, int i) {
+   if (i == 36) { // base caase
+      star2memory[m] = v;
+      return;
+   }
+   if (mask[i] == '0')
+      handleStar2Mask(mask, m, v, i + 1); //handle next bit
+   else if (mask[i] == '1') {
+      m |= (1LL << i);  //set bit i in memory location
+      handleStar2Mask(mask, m, v, i + 1);
+   } else {
+      m |= (1LL << i);  //set bit i in memory location
+      handleStar2Mask(mask, m, v, i + 1);
+      m &= ~(1LL << i); //clear bit i in memory location
+      handleStar2Mask(mask, m, v, i + 1);
+   }
+}
 
 void day14() {
    long long star1 = 0;
    long long star2 = 0;
+   map<long long, long long> star1memory;
+
    ifstream ifile("../day14.txt");
    string line;
    string mask;
-   map<long long, long long> memory;
    while (getline(ifile, line)) {
       string s;
-      int i;
-      int x;
-      int y;
-      char c;
-      double d;
       istringstream iline(line);
       iline >> s;
       if (s == "mask") {
          iline >> s >> s;
-         cout << s << endl;
-         mask = string(s.rbegin(), s.rend());
+         mask = string(s.rbegin(), s.rend()); //reverse so mask[i] is 2^i
       } else {
          istringstream reader(s.substr(4));
          long long loc;
          reader >> loc;
          long long value;
          iline >> s >> value;
-         cout << loc << " " << value << endl;
+         handleStar2Mask(mask, loc, value, 0);
          for(int i=0;i<36;++i) {
             if(mask[i]=='0')
                value &= ~(1ll<<i);
             else if (mask[i]=='1')
                value |= (1ll<<i);
          }
-         cout << "new " << value << endl;
-         memory[loc] = value;
+         star1memory[loc] = value;
       }
    }
-   for(auto [m,v]:memory)
+
+   for (auto [m, v] : star1memory)
       star1 += v;
+   for (auto [m, v] : star2memory)
+      star2 += v;
    cout << "Day 14 star 1 = " << star1 << "\n";
    cout << "Day 14 star 2 = " << star2 << "\n";
 }
