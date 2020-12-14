a21eaea59018f7464af4ebdd2c25772ef452032c
Chris Hartman <chris.m.hartman@gmail.com>
2020-12-13 20:44:26 -0900

    Remove extra #includes and usings

--- day14.cpp
+++ day14.cpp
@@ -1,42 +1,13 @@
-#include <algorithm>
-#include <cmath>
-#include <cstdlib>
 #include <fstream>
-#include <iomanip>
 #include <iostream>
-#include <iterator>
 #include <map>
-#include <memory>
-#include <numeric>
-#include <queue>
-#include <set>
 #include <sstream>
 #include <string>
-#include <tuple>
-#include <utility>
-#include <vector>
-using std::abs;
-using std::ceil;
 using std::cout;
-using std::endl;
-using std::forward_as_tuple;
 using std::ifstream;
-using std::istream;
 using std::istringstream;
 using std::map;
-using std::max;
-using std::max_element;
-using std::min;
-using std::pair;
-using std::queue;
-using std::set;
-using std::sqrt;
 using std::string;
-using std::swap;
-using std::tie;
-using std::tuple;
-
-using std::vector;
 
 map<long long, long long> star2memory;
 
